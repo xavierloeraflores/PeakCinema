366e7a8a0591e4dda895b3ea5b5a648b1ed6c6bb
Xavier Loera Flores <x.loera.tech@gmail.com>
2022-11-29 13:54:11 -0800

    init

---
+++ Shared/PeakCinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakCinemaApp.swift
+//  Shared
+//
+//  Created by Xavier Loera Flores on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PeakCinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

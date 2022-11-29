9eb40d05ce191e0cbe8b2a57118ad3b43c041d1c
Xavier Loera Flores <x.loera.tech@gmail.com>
2022-11-29 15:26:18 -0800

    added Vstack & added

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [.purple, .black]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            VStack{
+                VStack{
+                    Text("Peak Cinema")
+                        .font(.system(
+                            size: 32,
+                            weight: .bold,
+                            design: .monospaced)
+                        )
+                        .padding()
+                        .foregroundColor(.white)
+                    
+                    
+                    Image(systemName: "film.circle")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                LazyVGrid(columns: []) {
+                
+                }
+                
+            }
+        }
+    
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
